8b8abad2b14be2e2fe498b0309a30a7b346bb97f
Li <joy.li.ext@siemens.com>
2021-04-09 18:03:13 +0800

    ddd

--- SIFT/sift_calculate.py
+++ SIFT/sift_calculate.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2
 
-features = []
-queries = []
 sift_feature = []
 sift_image = []
 
@@ -14,59 +12,45 @@ class Result:
 def sortResults(r):
     return r.dis  
 
-output = open("ranklist.txt", "a")
+output = open(r"SIFT\ranklist.txt", "a")
 sift = cv2.xfeatures2d.SIFT_create()
 
 for i in range(1, 21):
     if(i<10):
-        querypath = r"D:\SIFT-CNN\CNN\VGG_features\Queries\0" + str(i) + ".npy"
         querysift = r"D:\SIFT-CNN\SIFT\SIFT_features\Queries\0" + str(i) + ".npy"
     else:
-        querypath = r"D:\SIFT-CNN\CNN\VGG_features\Queries\\" + str(i) + ".npy"
         querysift = r"D:\SIFT-CNN\SIFT\SIFT_features\Queries\\" + str(i) + ".npy"
-    queries.append(np.load(querypath))
     sift_feature.append(np.load(querysift))
 
 for i in range(1, 5001):
     if(i<10):
-        path = r"D:\SIFT-CNN\CNN\VGG_features\Images\000" + str(i) + ".npy"
         siftpath = r"D:\SIFT-CNN\SIFT\SIFT_features\Images\000" + str(i) + ".npy"
     elif (i<100):
-        path = r"D:\SIFT-CNN\CNN\VGG_features\Images\00" + str(i) + ".npy"
         siftpath = r"D:\SIFT-CNN\SIFT\SIFT_features\Images\00" + str(i) + ".npy"
     elif (i<1000):
-        path = r"D:\SIFT-CNN\CNN\VGG_features\Images\0" + str(i) + ".npy"
         siftpath = r"D:\SIFT-CNN\SIFT\SIFT_features\Images\0" + str(i) + ".npy"
     else:
-        path = r"D:\SIFT-CNN\CNN\VGG_features\Images\\" + str(i) + ".npy"
         siftpath = r"D:\SIFT-CNN\SIFT\SIFT_features\Images\\" + str(i) + ".npy"
-    features.append(np.load(path))
     sift_image.append(np.load(siftpath))
 
-queries = np.array(queries)
-features = np.array(features)
 sift_feature = np.array(sift_feature)
 sift_image = np.array(sift_image)
 
 i=1
-for q in queries:
+for q in sift_feature:
     output.write("Q"+str(i)+":")
     j=1
     results = []
 
-    for f in features:
+    for f in sift_image:
         bf = cv2.BFMatcher()
-        matches = bf.knnMatch(sift_feature[i - 1], sift_image[j - 1], k=2)
+        matches = bf.knnMatch(q, f, k=2)
         goodMatches = []
         for m,n in matches:
             if m.distance < 0.75*n.distance:
                 goodMatches.append([m])
-        similarity = len(goodMatches)
-        dis = np.linalg.norm(q-f)
-        if similarity > 100:
-            result = Result(j, 100*dis/(similarity))
-        else:
-            result = Result(j, dis)
+        similarity = len(goodMatches)/(len(matches)+1)
+        result = Result(j, 1-similarity)
         results.append(result)
         j+=1
     results = sorted(results, key = sortResults)
